e780263c596477297414d075312719dd8eee42d6
uyarmehmet1984 <144006189+uyarmehmet1984@users.noreply.github.com>
2024-02-20 00:27:05 +0300

    ss

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,12 @@ const userModel = require('../models/usermodels');
 
 // GET: Tüm kullanıcıları getir
 exports.getAllUsers = (req, res) => {
-    bookModel.getAllUsers((err, results) => {
+    userModel.getAllUsers((err, results) => {
       if (err) {
         res.status(500).send('Veritabanı hatası: ' + err.message);
       } else {
         res.json(results);
       }
-    });
+    });s
   };
   
